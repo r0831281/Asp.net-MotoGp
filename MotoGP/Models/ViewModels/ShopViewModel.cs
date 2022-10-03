02c5d4856f2981794d98873bce52d497641b39f3
r0831281 <r0831281@student.thomasmore.be>
2022-10-03 21:51:48 +0200

    init

---
+++ MotoGP/Models/ViewModels/ShopViewModel.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace MotoGP.Models.ViewModels
+{
+	public class ShopViewModel
+	{
+		public List<Race> Races { get; set; }
+		public SelectList Countries { get; set; }
+		public Ticket Ticket { get; set; }
+	}
+}
